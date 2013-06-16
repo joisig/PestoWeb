232292c85d862c06536f6330c392e7766d62823c
SuprDewd <suprdewd@gmail.com>
2013-06-16 18:12:56 +0000

    Update.

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-#    Sample main.py Tornado file
-#    (for Tornado on Heroku)
-#
-#    Author: Mike Dory | dory.me
-#    Created: 11.12.11 | Updated: 06.02.13
-#    Contributions by Tedb0t, gregory80
-#
-# ------------------------------------------
-
 #!/usr/bin/env python
 import os.path
 import tornado.escape
@@ -20,43 +11,36 @@ from tornado.options import define
 define("port", default=5000, help="run on the given port", type=int)
 
 
-# application settings and handle mapping info
 class Application(tornado.web.Application):
     def __init__(self):
+
         handlers = [
-            (r"/([^/]+)?", MainHandler)
+            (r"/?", MainHandler),
         ]
+
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             debug=True,
         )
+
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
-# the main page
 class MainHandler(tornado.web.RequestHandler):
-    def get(self, q):
-        if 'GOOGLEANALYTICSID' in os.environ:
-            google_analytics_id = os.environ['GOOGLEANALYTICSID']
-        else:
-            google_analytics_id = False
+    def get(self):
+        google_analytics_id = os.environ['GOOGLEANALYTICSID'] if 'GOOGLEANALYTICSID' in os.environ else False
 
         self.render(
             "main.html",
-            page_title='Heroku Funtimes',
-            page_heading='Hi!',
             google_analytics_id=google_analytics_id,
         )
 
 
-# RAMMING SPEEEEEEED!
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(tornado.options.options.port)
-
-    # start it up
     tornado.ioloop.IOLoop.instance().start()
 
 
